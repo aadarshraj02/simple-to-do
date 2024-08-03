5a035fc6b5c9f8caf8c770dc7e59d3a03a18d90d
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-03 19:41:01 +0530

    styled ibnput

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 export default function TodoInput(props) {
   const { handleAddTodos, todoValue, setTodoValue } = props;
   return (
-    <header>
+    <header className="flex justify-around w-full">
       <input
-        className="border-2 border-yellow-600"
+        className="border-2 border-orange-400 rounded-lg px-4 py-1 w-1/3"
         value={todoValue}
         onChange={(e) => {
           setTodoValue(e.target.value);
