0d819ae37c3099b2b4370898faa2e03c5c50f264
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-03 19:45:23 +0530

    styled add button

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -5,7 +5,7 @@ export default function TodoInput(props) {
   return (
     <header className="flex justify-around w-full">
       <input
-        className="border-2 border-orange-400 rounded-lg px-4 py-1 w-1/3"
+        className="border-2 border-orange-400 rounded-lg px-4 py-1 w-1/3 text-black"
         value={todoValue}
         onChange={(e) => {
           setTodoValue(e.target.value);
@@ -13,6 +13,7 @@ export default function TodoInput(props) {
         placeholder="Enter todo..."
       />
       <button
+        className="bg-orange-400 px-4 py-1 rounded-lg text-black hover:opacity-80"
         onClick={() => {
           handleAddTodos(todoValue);
           setTodoValue("");
