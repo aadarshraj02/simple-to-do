795eeea2f710fd08b207fbe514fee8f525cb7293
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-03 19:13:40 +0530

    added todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 const App = () => {
+  let todos = [
+    "Solve Leetcode in the morning",
+    "Code for 2 hours everyday",
+    "Learn new things daily",
+  ];
   return (
     <main>
       <TodoInput />
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,6 @@
 import TodoCard from "./TodoCard";
 
 const TodoList = () => {
-  let todos = [
-    "Solve Leetcode in the morning",
-    "Code for 2 hours everyday",
-    "Learn new things daily",
-  ];
   return (
     <div>
       {todos.map((todo, index) => (
