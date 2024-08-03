ab6c516ed4a9c157800ae8c17af1d9d9ec4fdbab
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-03 19:01:20 +0530

    added todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,8 @@
 const TodoList = () => {
+    let todos = ["Solve Leetcode in the morning",
+        "Code for 2 hours everyday",
+        "Learn new things daily"
+    ]
   return <div></div>;
 };
 
